d674e636f7d7e7dd341cae67c0b7c98c96b478c7
Dinesh Alapati <dine.alapati@gmail.com>
2017-10-18 00:52:07 -0400

    install maven in jenkins-master as part of docker build

--- jenkins-2.x/jenkins-master/jenkins-upgrade.sh
+++ jenkins-2.x/jenkins-master/jenkins-upgrade.sh
@@ -6,5 +6,5 @@ cd $DIR
 docker stop jenkins_master_1
 docker rm jenkins_master_1
 
-docker build -t jenkins_master -f Dockerfile --pull --no-cache .
-docker run --name=jenkins_master_1 -p 8080:8080 -p 50000:50000 --volumes-from=jenkins_data_1 -d jenkins_master
+docker build -t jenkins_master -f Dockerfile --pull --no-cache . || exit 1
+docker run --name=jenkins_master_1 -p 8088:8080 -p 50000:50000 --volumes-from=jenkins_data_1 -d jenkins_master || exit 1
