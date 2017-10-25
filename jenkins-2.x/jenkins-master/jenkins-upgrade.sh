1431c6c1cdb775fd506258f743cfdd49c5faa58d
Dinesh Alapati <dine.alapati@gmail.com>
2017-10-25 18:07:55 -0400

    update jenkins-master to use docker deamon from host machine

--- jenkins-2.x/jenkins-master/jenkins-upgrade.sh
+++ jenkins-2.x/jenkins-master/jenkins-upgrade.sh
@@ -7,4 +7,4 @@ docker stop jenkins_master_1
 docker rm jenkins_master_1
 
 docker build -t jenkins_master -f Dockerfile --pull --no-cache . || exit 1
-docker run --name=jenkins_master_1 -p 8088:8080 -p 50000:50000 --volumes-from=jenkins_data_1 -d jenkins_master || exit 1
+docker run --name=jenkins_master_1 -p 8088:8080 -p 50000:50000 --volumes-from=jenkins_data_1 -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -d jenkins_master || exit 1
